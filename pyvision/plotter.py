2361fcac1ec227f68f4e0607f5392f8ad97c2129
Marvin Teichmann <marvin.teichmann@googlemail.com>
2023-08-20 15:24:59 +0200

    Improve plotter to have good window titles.

--- pyvision/plotter.py
+++ pyvision/plotter.py
@@ -17,9 +17,11 @@ import scipy as scp
 
 from pyvision.logger import Logger
 
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
-                    level=logging.INFO,
-                    stream=sys.stdout)
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(message)s",
+    level=logging.INFO,
+    stream=sys.stdout,
+)
 
 from threading import Thread
 
@@ -33,8 +35,7 @@ default_conf = {
 
 
 def get_pyvision_plotter(conf, logdirs, names=None):
-
-    summary_name = 'summary.log.hdf5'
+    summary_name = "summary.log.hdf5"
 
     filenames = [os.path.join(name, summary_name) for name in logdirs]
     loggers = [Logger().load(file) for file in filenames]
@@ -72,12 +73,9 @@ class Plotter(object):
         logging.info(string)
 
     def plot_default(self, compact=False):
+        default = self.config["defaults"]
 
-        default = self.config['defaults']
-
-        iterator = zip(default['plot'],
-                       default['prefixes'],
-                       default['titles'])
+        iterator = zip(default["plot"], default["prefixes"], default["titles"])
 
         for key, prefix, title in iterator:
             self.plot(key, prefix=prefix, title=title)
@@ -86,15 +84,20 @@ class Plotter(object):
 
     def medianize(self, data, weight=20, unbiased=True):
         if unbiased:
-            medianized = [np.median(data[max(i - weight, 0): i + min(i, weight) + 1]) # NOQA
-                          for i in range(len(data))]
+            medianized = [
+                np.median(
+                    data[max(i - weight, 0) : i + min(i, weight) + 1]
+                )  # NOQA
+                for i in range(len(data))
+            ]
         else:
-            medianized = [np.median(data[max(i - weight, 0): i + 1])
-                          for i in range(len(data))]
+            medianized = [
+                np.median(data[max(i - weight, 0) : i + 1])
+                for i in range(len(data))
+            ]
         return medianized
 
     def plot(self, key, prefix=None, title=None, smoothed=True, percent=True):
-
         if prefix is not None:
             key = prefix + "\\" + key
 
@@ -104,19 +107,34 @@ class Plotter(object):
         data = [logger.data[key] for logger in self.loggers]
 
         if smoothed:
-            self.plot_data(self.steps, data, names=self.names,
-                           title=title, percent=percent)
+            self.plot_data(
+                self.steps,
+                data,
+                names=self.names,
+                title=title,
+                percent=percent,
+            )
         else:
-            self.plot_data(self.steps, data, names=self.names,
-                           title=title, marker=" ", linestyle='-',
-                           plot_smoothed=False, percent=percent)
+            self.plot_data(
+                self.steps,
+                data,
+                names=self.names,
+                title=title,
+                marker=" ",
+                linestyle="-",
+                plot_smoothed=False,
+                percent=percent,
+            )
+
+        # Set the window title
+        fig = plt.gcf()
+        fig.canvas.manager.set_window_title(title)
 
     def plot_miuo(self):
         self.plot(key="mIoU", prefix="val", title="Validation mIoU")
 
     def plot_accuracy(self):
-        self.plot(key="accuracy", prefix="val",
-                      title="Validation Accuracy")
+        self.plot(key="accuracy", prefix="val", title="Validation Accuracy")
 
     def plot_train_miuo(self):
         self.plot(key="mIoU", prefix="train", title="Train mIoU")
@@ -131,15 +149,26 @@ class Plotter(object):
     def get_logger(self):
         return self.loggers[0]
 
-    def plot_data(self, steps, plot_data, names,
-                  plot_smoothed=True, title=None, sm_weight=None,
-                  marker=".", linestyle=' ', annotate=True, percent=True,
-                  ax=None, show=True, show_legend=True):
-
+    def plot_data(
+        self,
+        steps,
+        plot_data,
+        names,
+        plot_smoothed=True,
+        title=None,
+        sm_weight=None,
+        marker=".",
+        linestyle=" ",
+        annotate=True,
+        percent=True,
+        ax=None,
+        show=True,
+        show_legend=True,
+    ):
         if len(steps) != len(names):
             steps = [steps for i in range(len(names))]
 
-        assert(len(names) == len(plot_data))
+        assert len(names) == len(plot_data)
 
         # plt.rcParams.update({'font.size': 14})
         # fig, ax = plt.subplots(figsize=(6.4, 4.8))
@@ -149,28 +178,34 @@ class Plotter(object):
         iterator = zip(plot_data, names, steps)
 
         for i, (data, name, mysteps) in enumerate(iterator):
-
             if percent:
                 p_data = [100 * d for d in data]
             else:
                 p_data = data
 
             try:
-                weight = self.config['num_smoothing_samples']
+                weight = self.config["num_smoothing_samples"]
             except KeyError:
                 weight = 20
 
             smoothed = self.medianize(p_data, weight=weight, unbiased=True)
 
-            color = 'C{}'.format(i)
+            color = "C{}".format(i)
 
             # Do plotting
-            ax.plot(mysteps, p_data, marker=marker, linestyle=linestyle,
-                    label=name + " (raw)", color=color)
+            ax.plot(
+                mysteps,
+                p_data,
+                marker=marker,
+                linestyle=linestyle,
+                label=name + " (raw)",
+                color=color,
+            )
 
             if plot_smoothed:
-                ax.plot(mysteps, smoothed,
-                        label=name + " (smooth)", color=color)
+                ax.plot(
+                    mysteps, smoothed, label=name + " (smooth)", color=color
+                )
 
                 if annotate:
                     self._do_annotation(ax, color, smoothed, mysteps, i)
@@ -178,8 +213,8 @@ class Plotter(object):
             if title is not None:
                 ax.set_title(title)
 
-            ax.set_xlabel('Epoch')
-            ax.set_ylabel('Score [%]')
+            ax.set_xlabel("Epoch")
+            ax.set_ylabel("Score [%]")
             # ax.legend()
             if show_legend:
                 ax.legend(loc=0)
@@ -190,17 +225,18 @@ class Plotter(object):
     def _do_annotation(self, ax, color, data, steps, iter):
         iter2 = 0
         for x_ann, y_ann, name in self.get_annotation_points(data, steps):
+            ax.plot([x_ann], [y_ann], "o", color="red")
 
-            ax.plot([x_ann], [y_ann], 'o', color='red')
-
-            ax.annotate('{}: {:.2f}'.format(name, y_ann),
-                        xy=(x_ann, y_ann),
-                        xytext=(-20 + 20 * iter2, 20 + 10 * iter2 + 20 * iter),
-                        textcoords='offset points',
-                        color=color, size=10,
-                        horizontalalignment='center',
-                        arrowprops=dict(arrowstyle="->",
-                                        alpha=0.75, color=color))
+            ax.annotate(
+                "{}: {:.2f}".format(name, y_ann),
+                xy=(x_ann, y_ann),
+                xytext=(-20 + 20 * iter2, 20 + 10 * iter2 + 20 * iter),
+                textcoords="offset points",
+                color=color,
+                size=10,
+                horizontalalignment="center",
+                arrowprops=dict(arrowstyle="->", alpha=0.75, color=color),
+            )
 
             iter2 = iter2 + 1
 
@@ -209,7 +245,7 @@ class Plotter(object):
         idmax = np.argmax(smoothed)
         xmax = steps[idmax]
 
-        p1 = (xmax, ymax, 'max')
+        p1 = (xmax, ymax, "max")
 
         if ymax == smoothed[-1]:
             return [p1]
@@ -217,25 +253,31 @@ class Plotter(object):
         ymin = np.min(smoothed[idmax:])
         xmin = np.argmin(smoothed[idmax:])
 
-        p2 = (xmax + steps[xmin], ymin, 'min')
+        p2 = (xmax + steps[xmin], ymin, "min")
 
         if ymin == smoothed[-1]:
             return [p1, p2]
 
         ylast = smoothed[-1]
         xlast = len(smoothed) - 1
-        p3 = (steps[xlast], ylast, 'last')
+        p3 = (steps[xlast], ylast, "last")
 
         return [p1, p2, p3]
 
 
 def medianize(data, weight=20, unbiased=True):
     if unbiased:
-        medianized = [np.median(data[max(i - weight, 0): i + min(i, weight) + 1]) # NOQA
-                      for i in range(len(data))]
+        medianized = [
+            np.median(
+                data[max(i - weight, 0) : i + min(i, weight) + 1]
+            )  # NOQA
+            for i in range(len(data))
+        ]
     else:
-        medianized = [np.median(data[max(i - weight, 0): i + 1])
-                      for i in range(len(data))]
+        medianized = [
+            np.median(data[max(i - weight, 0) : i + 1])
+            for i in range(len(data))
+        ]
     return medianized
 
 
@@ -253,12 +295,23 @@ class Plotter2(Plotter):
         if ax is None:
             self.fig, self.ax = plt.subplots()
 
-    def plot_scores(self, score, name, steps=None,
-                    plot_smoothed=True, sm_weight=20,
-                    marker=".", linestyle=' ', annotate=True, percent=True,
-                    show=False, show_legend=True, alpha=1,
-                    x_label='Epoch', y_label='Score [%]'):
-
+    def plot_scores(
+        self,
+        score,
+        name,
+        steps=None,
+        plot_smoothed=True,
+        sm_weight=20,
+        marker=".",
+        linestyle=" ",
+        annotate=True,
+        percent=True,
+        show=False,
+        show_legend=True,
+        alpha=1,
+        x_label="Epoch",
+        y_label="Score [%]",
+    ):
         ax = self.ax
 
         if steps is None:
@@ -269,16 +322,28 @@ class Plotter2(Plotter):
 
         smoothed = self.medianize(score, weight=sm_weight, unbiased=True)
 
-        color = 'C{}'.format(self.i)
+        color = "C{}".format(self.i)
         self.i += 1
 
         # Do plotting
-        ax.plot(steps, score, marker=marker, linestyle=linestyle,
-                label=name + " (raw)", color=color, alpha=alpha)
+        ax.plot(
+            steps,
+            score,
+            marker=marker,
+            linestyle=linestyle,
+            label=name + " (raw)",
+            color=color,
+            alpha=alpha,
+        )
 
         if plot_smoothed:
-            ax.plot(steps, smoothed,
-                    label=name + " (smooth)", color=color, alpha=alpha)
+            ax.plot(
+                steps,
+                smoothed,
+                label=name + " (smooth)",
+                color=color,
+                alpha=alpha,
+            )
 
             if annotate:
                 self._do_annotation(ax, color, smoothed, steps, self.i)
@@ -294,6 +359,5 @@ class Plotter2(Plotter):
             ax.legend(loc=0)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.info("Hello World.")
